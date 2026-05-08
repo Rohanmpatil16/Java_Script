33a5f22eb069bd96369e63385f551e8c412401d3
Rohanmpatil16 <rohanmpatil18@hgmail.com>
2026-05-08 21:02:49 +0530

    program of addition of array

---
+++ sum_array.js
@@ -0,0 +1,12 @@
+function findSum(arr) {
+    let sum = 0;
+
+    for (let i = 0; i < arr.length; i++) {
+        sum = sum + arr[i];
+    }
+
+    return sum;
+}
+
+
+console.log(findSum([1, 2, 3, 4]));
