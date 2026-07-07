aed41fbe211251417ac3df167d9336dfd720ec81
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-07-08 00:47:14 +0530

    Object using js

---
+++ object.js
@@ -0,0 +1,18 @@
+
+let emp={
+    name:"Rohan",
+    age:22,
+    city:"Pune",
+    company:"google"
+}
+
+console.log(emp);
+
+console.log(emp.name);
+
+delete emp.age;
+console.log(emp);
+
+
+emp.salary=10000000;
+console.log(emp);
