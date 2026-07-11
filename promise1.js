228f39538d5ab5339440aa7a2da6a9a0ab638663
Rohanmpatil16 <rohanmpatil18@gmail.com>
2026-07-11 22:59:54 +0530

    used promise method

---
+++ promise1.js
@@ -0,0 +1,16 @@
+function fetchData()
+{
+    return new Promise((resolve)=>{
+        setTimeout(()=>{
+            resolve("Data fetched successfully!");
+        }, 1000)
+    })
+}
+
+async function getData()
+{
+    const data = await fetchData();
+    console.log(data);
+
+}
+getData();
